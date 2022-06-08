8dab92c47477ec7d9127ed6b68b2d8a24bd6102b
Emily Daykin <emilydaykin31@gmail.com>
2022-06-08 22:39:25 +0200

    specified error messages

--- middleware/secureRoute.js
+++ middleware/secureRoute.js
@@ -7,19 +7,18 @@ const secureRoute = async (req, res, next) => {
   try {
     // Get jwt token from headers of request
     const authToken = req.headers.authorization;
-
     if (!authToken || !authToken.startsWith('Bearer')) {
-      return res.status(401).send({ message: 'Unauthorised' });
+      return res.status(401).send({ message: 'Unauthorised. No token or invalid token.' });
     } else {
       const token = authToken.replace('Bearer ', '');
       jwt.verify(token, secret, async (err, data) => {
         if (err) {
-          return res.status(400).send({ message: 'Unauthorised' });
+          return res.status(400).send({ message: 'Unauthorised. Token not verified.' });
         } else {
           console.log('data', data);
           const user = await User.findById(data.userId);
           if (!user) {
-            return res.status(401).send({ message: 'Unauthorised' });
+            return res.status(400).send({ message: 'Unauthorised. No user found.' });
           } else {
             req.currentUser = user;
             next();
