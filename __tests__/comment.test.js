69d3f71f0cdebc7bb27b5505ba58be0937ec34d8
Emily Daykin <emilydaykin31@gmail.com>
2022-06-11 19:20:21 +0200

    outlined new comments test suite

--- __tests__/comment.test.js
+++ __tests__/comment.test.js
@@ -2,14 +2,13 @@
 // rather than its own separate file, since it's not its own
 // model, but embedded in the Series one...
 
+// describe('Testing COMMENTS', () => {
 // TODO
 // it('Assert user can create a review on a series', async () => {});
-
 // TODO
 // it('Assert error when unauthenticated user tries to leave a review on a series', async () => {});
-
 // TODO
 // it('Assert user can delete their own reviews', async () => {});
-
 // TODO
 // it("Assert user can't delete others' reviews", async () => {});
+// });
